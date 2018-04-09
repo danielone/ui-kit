6092e789e9a74c2d29ab8f571577a4e7da6a968d
Poltergeist <philipph@mesosphere.io>
2018-04-09 15:10:30 +0200

    feat(badge): add example file for badge
    this adds a example file for the success state of the badge.

---
+++ components/badge/examples/success.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+import Badge from "../index";
+
+/** This is a example of the success badge */
+const Success: React.SFC = () => {
+  return <Badge appearance="success">Success</Badge>;
+};
+
+export default Success;
