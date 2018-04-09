99de2a3c4ab709f9ea6cc5ad743d2525e752109e
Poltergeist <philipph@mesosphere.io>
2018-04-09 15:08:36 +0200

    feat(docs): add generate component data script
    This adds a script to generate component data.

---
+++ scripts/generateComponentData.js
@@ -0,0 +1,122 @@
+var fs = require("fs");
+var path = require("path");
+var chalk = require("chalk");
+var parse = require("react-docgen-typescript").withCustomConfig(
+  path.join(__dirname, "../tsconfig.dist.json"),
+  {
+    skipPropsWithoutDoc: false
+  }
+).parse;
+var chokidar = require("chokidar");
+
+var paths = {
+  examples: path.join("examples"), // Examples are stored as a subdirectory in the component
+  components: path.join(__dirname, "../components"),
+  output: path.join(__dirname, "../config", "componentData.js")
+};
+
+const enableWatchMode = process.argv.slice(2) == "--watch";
+if (enableWatchMode) {
+  // Regenerate component metadata when components or examples change.
+  chokidar
+    .watch([paths.examples, paths.components])
+    .on("change", function(event, path) {
+      generate(paths);
+    });
+} else {
+  // Generate component metadata
+  generate(paths);
+}
+
+function generate(paths) {
+  var errors = [];
+  var componentData = getDirectories(paths.components)
+    .map(function(componentName) {
+      try {
+        return getComponentData(paths, componentName);
+      } catch (error) {
+        errors.push(
+          "An error occurred while attempting to generate metadata for " +
+            componentName +
+            ". " +
+            error
+        );
+      }
+    })
+    .reduce((memo, item) => memo.concat(item), []);
+  writeFile(
+    paths.output,
+    "module.exports = /* eslint-disable */ " +
+      JSON.stringify(errors.length ? errors : componentData)
+  );
+}
+
+function getComponentData(paths, componentName) {
+  let componentPath = path.join(
+    paths.components,
+    componentName,
+    componentName + ".tsx"
+  );
+  var content = readFile(path.join(componentPath));
+  var infos = parse(componentPath);
+  return infos.map(info => ({
+    name: componentName,
+    description: info.description,
+    props: info.props,
+    code: content,
+    examples: getExampleData(
+      path.join(paths.components, componentName, paths.examples),
+      componentName
+    )
+  }));
+}
+
+function getExampleData(examplesPath, componentName) {
+  var examples = getExampleFiles(examplesPath, componentName);
+  return examples.map(function(file) {
+    var filePath = path.join(examplesPath, file);
+    var content = readFile(filePath);
+    var info = parse(filePath)[0];
+    return {
+      // By convention, component name should match the filename.
+      // So remove the .js extension to get the component name.
+      name: file.slice(0, -4),
+      description: info.description,
+      code: content
+    };
+  });
+}
+
+function getExampleFiles(examplesPath, componentName) {
+  var exampleFiles = [];
+  try {
+    exampleFiles = getFiles(path.join(examplesPath));
+  } catch (error) {
+    console.log(chalk.red(`No examples found for ${componentName}.`));
+  }
+  return exampleFiles;
+}
+
+function getDirectories(filepath) {
+  return fs.readdirSync(filepath).filter(function(file) {
+    return fs.statSync(path.join(filepath, file)).isDirectory();
+  });
+}
+
+function getFiles(filepath) {
+  return fs.readdirSync(filepath).filter(function(file) {
+    return fs.statSync(path.join(filepath, file)).isFile();
+  });
+}
+
+function writeFile(filepath, content) {
+  fs.writeFile(filepath, content, function(err) {
+    err
+      ? console.log(chalk.red(err))
+      : console.log(chalk.green("Component data saved."));
+  });
+}
+
+function readFile(filePath) {
+  return fs.readFileSync(filePath, "utf-8");
+}
